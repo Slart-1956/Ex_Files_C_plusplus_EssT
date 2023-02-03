69a150474b75accf53a21e452b50b570289312db
AngerSoft <tapio.angervuori@kolumbus.fi>
2023-02-03 18:30:16 +0200

    Char02 tehty

---
+++ Exercise/test/pointers.cc
@@ -0,0 +1,17 @@
+// pointers.cpp by Bill Weinman [bw.org]
+// updated 2022-05-21
+#include <fmt/format.h>
+#include <iostream>
+
+using fmt::format;
+using std::cout;
+
+int main() {
+    int x {7};
+    int y {42};
+    int* ip = &x;
+
+    cout << format("The value of x is {}\n", x);
+    cout << format("The value of y is {}\n", y);
+    cout << format("The value of *ip is {}\n", *ip);
+}
