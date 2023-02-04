40fb62843b15a24ed06bef384ef923f5818dfd11
AngerSoft <tapio.angervuori@kolumbus.fi>
2023-02-04 02:41:08 +0200

    asfSDSd

--- Exercise/test/for.cc
+++ Exercise/test/for.cc
@@ -10,6 +10,6 @@ int main() {
     int array[] { 1, 2, 3, 4, 5 };
 
     for (int i {0}; i < 5; ++i) {
-        cout << format("element {} is {}\n", i, array[i]);
+        cout << format("element {} is {}\n", i, array[i]);  jgivboiov
     }
 }
